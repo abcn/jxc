c72b1e156f17cb7658fae0b159976419dd8d386d
zhouhaotong <zhouhaotong@163.com>
2016-02-05 11:44:12 +0800

    2

--- resources/views/backend/market/includes/partials/header-buttons.blade.php
+++ resources/views/backend/market/includes/partials/header-buttons.blade.php
@@ -1,7 +1,7 @@
     <div class="pull-right" style="margin-bottom:10px">
         <div class="btn-group">
           <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-              订单管理 <span class="caret"></span>
+              市场调查管理 <span class="caret"></span>
           </button>
           <ul class="dropdown-menu" role="menu">
               <li><a href="{{ route('admin.market') }}">所有调查</a></li>
